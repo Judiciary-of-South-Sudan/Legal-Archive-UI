8fe4d84519462b4bf2e5ed2ed7acafe3109e3ead
Munyijwok James Alala <habib.munji@gmail.com>
2026-06-14 02:41:33 +0300

    Wrap App component with BandwidthProvider for managing bandwidth state

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n'
+import { BandwidthProvider } from './contexts/BandwidthContext.tsx'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BandwidthProvider>
+    <App />
+  </BandwidthProvider>
+);
