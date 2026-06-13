a8a11d87a8ee538355e6ac550863069ea1e4d868
Munyijwok James Alala <habib.munji@gmail.com>
2026-06-14 01:52:21 +0300

    Fix JudgmentDetail component: update margin class for loading text alignment

--- src/pages/judgments/JudgmentDetail.tsx
+++ src/pages/judgments/JudgmentDetail.tsx
@@ -105,7 +105,7 @@ const JudgmentDetail: React.FC = () => {
         <Header />
         <main className="container mx-auto px-4 py-12 flex items-center justify-center">
           <Loader2 className="h-8 w-8 animate-spin text-primary" />
-          <span className="ml-2">{t('judgments.loading_detail')}</span>
+          <span className="ms-2">{t('judgments.loading_detail')}</span>
         </main>
         <Footer />
       </div>
